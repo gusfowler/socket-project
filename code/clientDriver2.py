b9d23a5d506041a6ea87faa4b71cfe616f553701
Gus Fowler <gus.fowler12@gmail.com>
2022-03-04 21:25:17 +0000

    testing port generation

---
+++ code/clientDriver2.py
@@ -0,0 +1,30 @@
+import sys
+import socket
+from time import sleep
+
+import Connection
+#from ManagePeer2Peer import Player
+
+IP = Connection.getIP()
+PORT = 23000
+
+#if len(sys.argv > 0):
+#    for arg in sys.argv:
+#        if arg == '-a':
+#            IP = sys.argv[sys.argv.where('-a') + 1]
+#        elif arg == '-p':
+#            PORT = sys.argv[sys.argv.where('-p') + 1]
+
+sleep(3)
+print("My IP is:\t", IP)
+IP = socket.gethostbyname("socket-project_server_1")
+#IP = '172.19.0.3'
+print("Server IP is:\t", IP)
+server = Connection.Client(IP, PORT)
+
+
+while True:
+    if len(server.recvBuffer) > 0: 
+        print("Driver:\t", server.getMsgs())
+        server.sendMsg("REGISTER cole")
+    sleep(Connection.SLEEP_TIME)
